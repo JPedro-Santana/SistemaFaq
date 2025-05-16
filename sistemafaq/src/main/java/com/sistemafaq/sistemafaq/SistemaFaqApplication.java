51c1cd3f31de9f0be8704b78bd30664caf176efe
João Pedro Santana <joao.pedrosousa5@gmail.com>
2025-05-16 16:45:07 -0300

    Iniciando back-end
    Iniciando projeto com Spring

---
+++ sistemafaq/src/main/java/com/sistemafaq/sistemafaq/SistemaFaqApplication.java
@@ -0,0 +1,13 @@
+package com.sistemafaq.sistemafaq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemafaqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemafaqApplication.class, args);
+	}
+
+}
---
+++ sistemafaq/src/test/java/com/sistemafaq/sistemafaq/SistemafaqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistemafaq.sistemafaq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemafaqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
