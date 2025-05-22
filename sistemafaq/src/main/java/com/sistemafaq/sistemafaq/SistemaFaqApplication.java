00273756f84d03fed2a91b287e86f01ae5060d7a
João Pedro Santana <joao.pedrosousa5@gmail.com>
2025-05-22 19:14:26 -0300

    Iniciando o front-end, e configurando os models e controller

--- sistemafaq/src/main/java/com/sistemafaq/sistemafaq/SistemaFaqApplication.java
+++ sistemafaq/src/main/java/com/sistemafaq/sistemafaq/SistemaFaqApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SistemafaqApplication {
+public class SistemaFaqApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SistemafaqApplication.class, args);
+		SpringApplication.run(SistemaFaqApplication.class, args);
 	}
 
 }
